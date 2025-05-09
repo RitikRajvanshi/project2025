465bf8acb25678b3071cf997449c5601353ef6f3
ritik.rajvanshi <ritik.rajvanshi@apvtechnologies.com>
2025-05-09 10:03:13 +0530

    just for check.

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -3,8 +3,8 @@ const {Pool} = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'project',
-    password: '8538', 
+    database: 'dummy',
+    password: '3009', 
     port: 5432,
     max: 20,
     idleTimeoutMillis: 30000,
