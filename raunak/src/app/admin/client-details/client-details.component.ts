d147e4a00ce6ad84cda3cf7bb42df183c93486ad
ritik.rajvanshi <ritik.rajvanshi@apvtechnologies.com>
2025-05-21 22:59:36 +0530

    Coins addition.

--- raunak/src/app/admin/client-details/client-details.component.ts
+++ raunak/src/app/admin/client-details/client-details.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { debounceTime } from 'rxjs/operators';
-import { Component , ViewChild, ElementRef} from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -22,28 +22,29 @@ export class ClientDetailsComponent {
   id: Number = 0;
   clientDataArray: any;
   passwordForm: any;
-  coinsForm:any;
+  coinsForm: any;
 
   passobj = {
-    newPass:'',
-    changePass:'',
-    user_id:0
+    newPass: '',
+    changePass: '',
+    user_id: 0
   }
-  coinUpdate={
-    user_id:0,                  //parentid
-    user_code:'',
-    parent_id:0,
-    parent_code:'',
-    parent_account_balance:0,
-    account_balance:0,
-    new_coins:0
+  coinUpdate = {
+    user_id: 0,                  //parentid
+    user_code: '',
+    parent_id: 0,
+    parent_code: '',
+    parent_account_balance: 0,
+    account_balance: 0,
+    new_coins: 0
   }
 
-  user_id:any=0;
+  user_id: any = 0;
 
-  parrentaccount=0;
-  useraccount=0;
-  disableSubmit=false;
+  parrentaccount = 0;
+  useraccount = 0;
+  disableSubmit = false;
+  isDeposit: boolean = true;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {
 
@@ -57,12 +58,13 @@ export class ClientDetailsComponent {
     }
     this.changePass();
     this.addcoinsform();
-      this.coinsForm.get('new_coins')?.valueChanges
-    .pipe(debounceTime(500))
-    .subscribe((value:any) => {
-      const newCoins = Number(value);
-      this.processCoinUpdate(newCoins);
-    });
+
+    this.coinsForm.get('new_coins')?.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe((value: any) => {
+        const newCoins = Number(value);
+        this.processCoinUpdate(newCoins);
+      });
 
   }
 
@@ -89,31 +91,33 @@ export class ClientDetailsComponent {
     }
   }
 
-  getparentdatabyuserid(user_id:any){
+  getparentdatabyuserid(user_id: any) {
     console.log(user_id, "userid")
     const url = environment.ADMIN_URL + environment.ADMIN.GET_PARENT_USER_DATA_BY_USERID;
-    this.http.post(url,user_id).subscribe((results:any)=>{
+
+    this.http.post(url, user_id).subscribe((results: any) => {
       console.log(results, "parentdata");
       this.coinUpdate.parent_code = results[0]?.parent_code;
       this.coinUpdate.parent_account_balance = results[0]?.parent_account_balance;
       this.parrentaccount = results[0]?.parent_account_balance;
     })
-    }
 
-    clientData(created_by:any){
-      const url = environment.ADMIN_URL + environment.ADMIN.GET_USERS_HIERARCHICAL_DATA;
-      console.log(url, "URL")
-      this.http.post<any>(url, created_by).subscribe((results: any) => {
-        console.log(results, "gethierarhchaldata");
-        // this.subAdminDataArray = results;
-        
-        this.clientDataArray = results.filter((data: any) => {
-          return data.level == 7;
-        });
-    
-      })
-    }
-    
+  }
+
+  clientData(created_by: any) {
+    const url = environment.ADMIN_URL + environment.ADMIN.GET_USERS_HIERARCHICAL_DATA;
+    console.log(url, "URL")
+    this.http.post<any>(url, created_by).subscribe((results: any) => {
+      console.log(results, "gethierarhchaldata");
+      // this.subAdminDataArray = results;
+
+      this.clientDataArray = results.filter((data: any) => {
+        return data.level == 7;
+      });
+
+    })
+  }
+
   updateStatus(data: any) {
     const url = environment.ADMIN_URL + environment.ADMIN.UPDATE_STATUS;
     var updatedStatus = 0;
@@ -135,7 +139,7 @@ export class ClientDetailsComponent {
     })
   }
 
-  
+
   accountLock(data: any) {
     const url = environment.ADMIN_URL + environment.ADMIN.ACCOUNT_LOCK;
     var updatedAccountStatus = 0;
@@ -185,123 +189,139 @@ export class ClientDetailsComponent {
   //   }
   // }
 
-  sendinguser(data:any){
+  sendinguser(data: any, operation: string) {
     this.passobj.user_id = data.user_id;
     console.log(data, "dataforcoins")
     this.coinUpdate.user_id = data.user_id;
+    // operation === 'Deposit' ? this.isDeposit == true : this.isDeposit == false;
+    this.isDeposit = operation === 'Deposit';
     this.coinUpdate.parent_id = data.created_by;
+
     this.coinUpdate.user_code = data.user_code;
     this.coinUpdate.account_balance = data.account_balance;
     this.useraccount = data.account_balance;
+    console.log(this.isDeposit, "this.isDeposit")
     setTimeout(() => {
-    this.getparentdatabyuserid(this.coinUpdate);
+      this.getparentdatabyuserid(this.coinUpdate);
     }, 500);
   }
 
   changepassword() {
-  console.log(this.passobj, "password change object");
-  const url = environment.ADMIN_URL+environment.ADMIN.CHANGE_PASSWORD;
-  this.http.post(url,this.passobj).subscribe((result:any)=>{
-    Swal.fire({
-      icon: "success",
-      title: "Password Changed!",
-      showConfirmButton: false,
-      timer: 1000
-    }).then(()=>{
-      this.changePass();
-      setTimeout(() => {
-        this.closebutton.nativeElement.click();
-      }, 100); // Adjust the delay time as needed
+    console.log(this.passobj, "password change object");
+    const url = environment.ADMIN_URL + environment.ADMIN.CHANGE_PASSWORD;
+    this.http.post(url, this.passobj).subscribe((result: any) => {
+      Swal.fire({
+        icon: "success",
+        title: "Password Changed!",
+        showConfirmButton: false,
+        timer: 1000
+      }).then(() => {
+        this.changePass();
+        setTimeout(() => {
+          this.closebutton.nativeElement.click();
+        }, 100); // Adjust the delay time as needed
+      })
     })
-  })
   }
 
- dynamicCoins(event: Event){
-  const newCoins:any = Number(this.coinsForm.get('new_coins').value);
-  
-  if(this.parrentaccount < newCoins){
-    event.preventDefault();
-    this.disableSubmit = true;    
+  dynamicCoins(event: Event) {
+    const newCoins: any = Number(this.coinsForm.get('new_coins').value);
+
+    if (this.parrentaccount < newCoins) {
+      event.preventDefault();
+      this.disableSubmit = true;
+    }
+    this.processCoinUpdate(newCoins);
   }
-  this.processCoinUpdate(newCoins);
- }
 
- processCoinUpdate(newCoins: number) {
-  if (this.parrentaccount < newCoins) {
-    this.disableSubmit = true;
-  } else {
-    this.disableSubmit = false;
+  processCoinUpdate(newCoins: number) {
+    if (this.parrentaccount < newCoins) {
+      this.disableSubmit = true;
+    } else {
+      let Accountbalance: number;
+      let newAccountbalance: number;
 
-    const Accountbalance:any = Number(this.parrentaccount) - newCoins;
-    const newAccountbalance:any = Number(this.useraccount) + newCoins;
+      this.disableSubmit = false;
 
-    this.coinUpdate.parent_account_balance = +Accountbalance.toFixed(2);
-    this.coinUpdate.account_balance = +newAccountbalance.toFixed(2);
+      if (this.isDeposit) {
+        Accountbalance = Number(this.parrentaccount) - newCoins;
+        newAccountbalance = Number(this.useraccount) + newCoins;
+      }
+      else {
+        Accountbalance = Number(this.parrentaccount) - newCoins;
+        newAccountbalance = Number(this.useraccount) + newCoins;
+      }
+      this.coinUpdate.parent_account_balance = +Accountbalance.toFixed(2);
+      this.coinUpdate.account_balance = +newAccountbalance.toFixed(2);
+    }
   }
-}
 
 
- 
-dynamicCoins2(event: Event){
-  if(this.useraccount < this.coinUpdate.new_coins){
-    event.preventDefault();
-    this.disableSubmit = true;    
+
+  dynamicCoins2(event: Event) {
+    if (this.useraccount < this.coinUpdate.new_coins) {
+      event.preventDefault();
+      this.disableSubmit = true;
+    }
+    else {
+      this.disableSubmit = false;
+      this.coinUpdate.parent_account_balance = this.parrentaccount + this.coinUpdate.new_coins;
+      this.coinUpdate.account_balance = this.useraccount - this.coinUpdate.new_coins;
+    }
   }
-  else{
-    this.disableSubmit = false;
-    this.coinUpdate.parent_account_balance = this.parrentaccount + this.coinUpdate.new_coins;
-    this.coinUpdate.account_balance = this.useraccount - this.coinUpdate.new_coins;
-  }
- }
 
-  addcoins(){
+  addcoins() {
+
     const url = environment.ADMIN_URL + environment.ADMIN.DEPOSIT_COINS;
-    this.http.post(url,this.coinUpdate).subscribe((results:any)=>{
+    console.log(this.coinUpdate, "this.coinUpdate")
+
+    this.http.post(url, this.coinUpdate).subscribe((results: any) => {
       console.log(results);
       Swal.fire({
         icon: "success",
         title: "Coins Deposited Successfully!",
         showConfirmButton: false,
         timer: 1000
-      }).then(()=>{
+      }).then(() => {
         this.addcoinsform();
         this.getUserdata();
-                setTimeout(() => {
+
+        setTimeout(() => {
           this.closebutton2.nativeElement.click();
         }, 100);
-      
-    })
-  })
-}
 
-substractCoins(){
-  const url = environment.ADMIN_URL + environment.ADMIN.DEPOSIT_COINS;
-    this.http.post(url,this.coinUpdate).subscribe((results:any)=>{
+      })
+    })
+  }
+
+  substractCoins() {
+    const url = environment.ADMIN_URL + environment.ADMIN.DEPOSIT_COINS;
+    this.http.post(url, this.coinUpdate).subscribe((results: any) => {
       console.log(results);
       Swal.fire({
         icon: "success",
         title: "Coins withdraw Successfully!",
         showConfirmButton: false,
         timer: 1000
-      }).then(()=>{
+      }).then(() => {
         this.addcoinsform();
         this.getUserdata();
         setTimeout(() => {
           this.closebutton3.nativeElement.click();
         }, 100);
-      
+
+      })
     })
-  })
-}
+  }
 
   changePass() {
     this.passwordForm = new FormGroup({
       newPass: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(7)]),
-      confirmPass: new FormControl('', [Validators.required, this.passwordMatchValidator(),Validators.minLength(4), Validators.maxLength(7)])
+      confirmPass: new FormControl('', [Validators.required, this.passwordMatchValidator(), Validators.minLength(4), Validators.maxLength(7)])
     })
   }
 
-  addcoinsform(){
+  addcoinsform() {
     this.coinsForm = new FormGroup({
       new_coins: new FormControl(0, [Validators.required]),
     })
@@ -312,7 +332,7 @@ substractCoins(){
       const password = control.root.get('newPass');
       const confirmPassword = control.root.get('confirmPass');
 
-     // Check if both fields are non-empty before performing match validation
+      // Check if both fields are non-empty before performing match validation
       if (password && confirmPassword && confirmPassword.value && password.value !== confirmPassword.value) {
         confirmPassword.setErrors({ 'passwordMismatch': true });
         return { 'passwordMismatch': true };
@@ -323,11 +343,11 @@ substractCoins(){
     };
   }
 
-  close(){
+  close() {
     this.passobj = {
-      newPass:'',
-      changePass:'',
-      user_id:0
+      newPass: '',
+      changePass: '',
+      user_id: 0
     }
 
     this.passwordForm.get('newPass').markAsUntouched();
